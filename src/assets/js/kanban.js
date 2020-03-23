ec54f1953b83b22a962e0df550db05a0fb5abd16
pvrvd0x <d3m35s1ah@protonmail.com>
2020-03-23 20:26:08 +0300

    Clean repo

--- src/assets/js/kanban.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    'use strict';
-    if ($("#kanban-board-wrapper").length) {
-        $("#board-category_1,#board-category_2,#board-category_3").sortable({
-            connectWith: "#board-category_1,#board-category_2,#board-category_3",
-            items: ".task-item"
-        });
-    }
-});
